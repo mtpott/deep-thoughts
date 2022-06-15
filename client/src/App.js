8467a48714eede608341ee1375a28a2e628329a4
megan <mtpott23@gmail.com>
2022-06-15 09:26:44 -0400

    routes connected successfully

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Home from './pages/Home';
 
@@ -17,13 +23,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
       <div className='flex-column justify-flex-start min-100-vh'>
         <Header />
         <div className='container'>
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="/thought/:id" element={<SingleThought />} />
+
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
         </div>
         <Footer />
       </div>
+      </Router>
     </ApolloProvider>
   );
 }
